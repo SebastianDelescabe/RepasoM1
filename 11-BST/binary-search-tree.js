185584c04c0dea2ec7e945158a867e5330bcf5e5
SebastianDelescabe <sebastiandelescabe@gmail.com>
2021-05-18 01:37:43 -0300

    nuevo

--- 11-BST/binary-search-tree.js
+++ 11-BST/binary-search-tree.js
@@ -1,30 +1,45 @@
-  function Node(value){
-    this.value = value;
-    this.left = null;
-    this.right = null;
+function Node(value){
+  this.value = value;
+  this.left = null;
+  this.right = null;
 }
 
 function BinarySearchTree() {
   this.root = null;
   }
 
-BinarySearchTree.prototype.insert = function(value) {
+BinarySearchTree.prototype.insert = function(value) {   //Hecho con root
+  var nodo = new Node(value)
   if(!this.root){
-    this.root = value;
+    this.root = nodo;
     return this.root;
   }
-  if(value < this.value){
+  if(nodo < this.value){
     if(!this.left){
       this.left = new Node(value)
     }else{
-      while(this.left){
-        this.left = value
-      }
+      this.left = insert(value)
+    }
+  }else{
+    if(!this.right){
+      this.right = new Node(value)
+    }else {
+      this.right = insert(value)
     }
   }
 }
 
+// var arbolito = new BinarySearchTree()
+// arbolito.insert(20)
+// arbolito.insert(15)
+// arbolito.insert(25)
+// arbolito.insert(10)
+// arbolito.insert(30)
+
 BinarySearchTree.prototype.find= function(value) {
+  var nodo = new Node(value)
+  if(nodo === this.value) return true;
+  
   // escribe un metodo que busque un nodo especifico
 }
 
